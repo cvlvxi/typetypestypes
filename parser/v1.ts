1b5c2abbbac2e7f9cb69bb098fd9a759331f0a95
cvlvxi <cvlvxi@outlook.com>
2021-10-29 15:51:06 +1100

    fmt

--- parser/v1.ts
+++ parser/v1.ts
@@ -1,13 +1,13 @@
 // deno-lint-ignore-file  no-unused-vars
 /********************************************************************************
- * TypeParser Version 1: 
+ * TypeParser Version 1:
  *  - Simple Parser pattern
 /********************************************************************************
  */
 
 type TypeParsed<T> = { parsed: true; value: T } | {
-	parsed: false;
-	reason: string;
+  parsed: false;
+  reason: string;
 };
 
 // Assume input will always come in as string
@@ -18,44 +18,43 @@ type TypeCallback<T> = (input: string) => TypeParsed<T[keyof T]>;
 // Inflight interface of all T's fields to something that returns TypeParse<T>
 // TypeParse<T> will be defined by parsing functions
 type TypeCallbacks<T> = {
-	[K in keyof T]: TypeCallback<T>;
+  [K in keyof T]: TypeCallback<T>;
 };
 
 type TypeCollection<T> = {
-	[K in keyof T]: { input: string; callback: TypeCallback<T> };
+  [K in keyof T]: { input: string; callback: TypeCallback<T> };
 };
 
 function collect<T>(
-	inputs: TypeInput<T>,
-	callbacks: TypeCallbacks<T>,
+  inputs: TypeInput<T>,
+  callbacks: TypeCallbacks<T>,
 ): TypeCollection<T> {
-	type keyT = keyof T;
-	const keyInputCallbackArray = (Object.keys(inputs) as Array<keyT>).map(
-		(key) => {
-			return [key, {
-				input: inputs[key],
-				callback: callbacks[key],
-			}];
-		},
-	);
-	return Object.fromEntries(
-		keyInputCallbackArray,
-	) as unknown as TypeCollection<T>;
+  type keyT = keyof T;
+  const keyInputCallbackArray = (Object.keys(inputs) as Array<keyT>).map(
+    (key) => {
+      return [key, {
+        input: inputs[key],
+        callback: callbacks[key],
+      }];
+    },
+  );
+  return Object.fromEntries(
+    keyInputCallbackArray,
+  ) as unknown as TypeCollection<T>;
 }
 
 function parse<T>(collection: TypeCollection<T>) {
-	type keyT = keyof T;
-	type callbackResultT = TypeParsed<T[keyT]>;
-	for (const key of (Object.keys(collection) as Array<keyT>)) {
-		const { input, callback }: {
-			input: string;
-			callback: TypeCallback<T>;
-		} = collection[key];
-		const res: callbackResultT = callback(input);
-		console.log(res);
-	}
+  type keyT = keyof T;
+  type callbackResultT = TypeParsed<T[keyT]>;
+  for (const key of (Object.keys(collection) as Array<keyT>)) {
+    const { input, callback }: {
+      input: string;
+      callback: TypeCallback<T>;
+    } = collection[key];
+    const res: callbackResultT = callback(input);
+    console.log(res);
+  }
 }
-
 
 /********************************************************************************
  * Data Input
@@ -66,22 +65,22 @@ function parse<T>(collection: TypeCollection<T>) {
 /********************************************************************************
  */
 type Data = {
-	dog: string;
-	cat: number;
+  dog: string;
+  cat: number;
 };
 
 const dataInput: TypeInput<Data> = {
-	dog: 'hello',
-	cat: 'goodbye',
+  dog: "hello",
+  cat: "goodbye",
 };
 
 const dataFuncs: TypeCallbacks<Data> = {
-	dog: (x) => {
-		return { parsed: true, value: 'ok' };
-	},
-	cat: (x) => {
-		return { parsed: true, value: 1 };
-	},
+  dog: (x) => {
+    return { parsed: true, value: "ok" };
+  },
+  cat: (x) => {
+    return { parsed: true, value: 1 };
+  },
 };
 
 const collectData = collect<Data>(dataInput, dataFuncs);
