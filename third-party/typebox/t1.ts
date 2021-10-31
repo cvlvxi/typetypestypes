b21377f9fd7b6e8aeb0399b5ce5ca1e18ef1c61d
cvlvxi <cvlvxi@outlook.com>
2021-10-31 14:22:55 +1100

    add rescript

--- third-party/typebox/t1.ts
+++ third-party/typebox/t1.ts
@@ -1,8 +1,9 @@
-import { Type, Static } from "./typebox/src/typebox.ts"
+import { Static, Type } from "./typebox/src/typebox.ts";
 
-const Node = Type.Rec(Self => Type.Object({
+const Node = Type.Rec((Self) =>
+  Type.Object({
     nodeId: Type.String(),
-    nodes: Type.Array(Self)
-}, { additionalProperties: false }), { $id: 'Node' })
+    nodes: Type.Array(Self),
+  }, { additionalProperties: false }), { $id: "Node" });
 
-console.log(Node)
+console.log(Node);
